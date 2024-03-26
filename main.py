e035fc764102d8df43db751f556dcc9385e51d32
Tapiwa <mazaruratapiwa03@gmail.com>
2024-03-26 10:29:02 +0200

    Tuesday

--- main.py
+++ main.py
@@ -16,66 +16,78 @@ y_values_actual = np.matmul(DesignMatrix, Theta)
 noise = np.random.normal(0,8,150)
 y_values = y_values_actual+ noise
 #v
-# plt.scatter(X , y_values_actual)
+plt.scatter(X , y_values_actual)
+# plt.show()
+# vi
+X_training = DesignMatrix[:100]
+y_training = y_values[:100]
+X_validate = DesignMatrix[100: 125]
+y_validate = y_values[100: 125]
+X_Test = DesignMatrix[125:]
+y_test = y_values[125]
+print(X_Test)
 
 
 # B)
-X_training = X[:100]
-X_validate = X[100: 125]
-X_Test = X[125:]
+# X_training = X[:100]
+# X_validate = X[100: 125]
+# X_Test = X[125:]
 
-Design_Matrix_training = DesignMatrix[:100]
-Desing_Matrix_validate = DesignMatrix[100: 125]
-Desing_Matrix_Test = DesignMatrix[125:]
+# Design_Matrix_training = DesignMatrix[:100]
+# Desing_Matrix_validate = DesignMatrix[100: 125]
+# Desing_Matrix_Test = DesignMatrix[125:]
 
-y_values_training = y_values[:100]
-y_values_validate = y_values[100: 125]
-y_values_Test = y_values[125:]
+# y_values_training = y_values[:100]
+# y_values_validate = y_values[100: 125]
+# y_values_Test = y_values[125:]
 
-Xtx = np.linalg.inv(np.matmul(Design_Matrix_training.transpose() , Design_Matrix_training))
-xty = np.matmul(Design_Matrix_training.transpose() , y_values_training)
-Theta_new = np.matmul( Xtx,xty) 
-#ii The theta's are really close to each other
+# Xtx = np.linalg.inv(np.matmul(Design_Matrix_training.transpose() , Design_Matrix_training))
+# xty = np.matmul(Design_Matrix_training.transpose() , y_values_training)
+# Theta_new = np.matmul( Xtx,xty) 
+# #ii The theta's are really close to each other
 
-# print(Theta_new)
-# print(Theta)
+# # print(Theta_new)
+# # print(Theta)
 
-#iii
-Error = (1/2)*(np.sum((y_values_actual[:100] - y_values_training)**2))
-print(Error)
+# #iii
+# Error = (1/2)*(np.sum((y_values_actual[:100] - y_values_training)**2))
+# # print(Error)
 
 
-#iv 
-# y_values_actual = np.matmul(DesignMatrix, Theta_new)
-X_training.sort()
-Design_Matrix_training = Design_Matrix_training[Design_Matrix_training[:,1].argsort()]
-y_values_training = np.matmul(Design_Matrix_training, Theta) + noise[:100]
-y_values_actual_sorted = np.matmul(Design_Matrix_training, Theta)
-# plt.plot( X_training , y_values_training, color = "red")
-# plt.show()
+# #iv 
+# # y_values_actual = np.matmul(DesignMatrix, Theta_new)
+# X_training.sort()
+# Design_Matrix_training = Design_Matrix_training[Design_Matrix_training[:,1].argsort()]
+# y_values_training = np.matmul(Design_Matrix_training, Theta) + noise[:100]
+# y_values_actual_sorted = np.matmul(Design_Matrix_training, Theta)
+# # plt.plot( X_training , y_values_training, color = "red")
+# # plt.show()
 
-#V
-alpha = 0.0002
+# #V
+alpha = 0.2
 
-Theta_old = Theta
+Theta_old = np.array(Theta)
+Theta_old[0] += 1  
 e = 0.0000000000000000000000000000000000000000000005
-Error_array =  np.array([Error])
+# Error_array =  np.array([Error])
 Time_Step = np.array([0])
 count=1
 
-while np.linalg.norm(abs(Theta - Theta_old)) < e:
-    partial_Error =np.matmul((y_values_training - y_values_actual_sorted[:100]), X_training)
-    Theta = Theta_old - alpha*partial_Error
-    Theta_old = Theta
-    count+=1
-    y_values_training = np.matmul(Design_Matrix_training, Theta)
-    if count%20 ==0 :
-        Error = (1/2)*(np.sum((y_values_actual_sorted[:100] - y_values_training)**2))
-        Error_array = np.append(Error_array, Error)
-        Time_Step = np.append(Time_Step,count)
-print(Time_Step)
-print(Error_array)
-plt.plot(Time_Step, Error_array)
+def descend(x,y,theta,alpha):
+    dTheta = np.zeros(len(theta))
+    N= len(x)
+    for xi,yi in zip(x,y):
+        for i in range(len(dTheta)):
+            dTheta[i] = dTheta[i] -2*xi[i] * (yi - np.dot(theta,xi))  
 
-plt.show()
+    theta = theta - alpha *(1/N) * dTheta
+    return theta
+
+while not np.array_equal(Theta,Theta_old):
+    Theta_old = np.array(Theta)
+    Theta = descend(X_training,y_training,Theta,alpha) 
+    count +=1
+    print(count, ". Theta: ", Theta , " ThetaOld: " , Theta_old)
+
+# plt.show()
 
